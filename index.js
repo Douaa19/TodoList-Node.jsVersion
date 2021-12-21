14f6072b90691d1183d339cdb6691dbc1d2b0602
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-12-21 15:01:47 +0100

    developpementRouter

--- index.js
+++ index.js
@@ -8,45 +8,59 @@ const hostName = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
 
     res.setHeader('Content-Type', 'text/html');
 
-    let path = './views/';
-    switch(req.url) {
-        case '/':
-            path += 'index.html';
+    if (req.url.endsWith('.html')) {
+        const dir = req.url.slice(1, 7);
+        const nameFile = req.url.slice(7);
+        fs.stat(`./${dir}${nameFile}`, (err, stats) => {
             res.statusCode = 200;
-            break;
-        case '/about':
-            path += 'about.html';
-            res.statusCode = 200;
-            break;
-        case '/contact':
-            path += 'contact.html';
-            res.statusCode = 200;
-            break;
-        // Redirect page
-        case '/about-us':
-            res.statusCode = 301;
-            res.setHeader('Location', '/about');
-            res.end();
-            break;
-        default:
-            path += '404.html';
-            res.statusCode = 404;
-            break;
+            res.setHeader('Content-Type', 'text/html');
+            if (stats) {
+                fs.createReadStream(dir + nameFile).pipe(res);
+            } else {
+                res.statusCode = 404;
+                res.end('Sorry, page does not found');
+            }
+        })
     }
+
+    // let path = './views/';
+    // switch(req.url) {
+    //     case '/':
+    //         path += 'index.html';
+    //         res.statusCode = 200;
+    //         break;
+    //     case '/about':
+    //         path += 'about.html';
+    //         res.statusCode = 200;
+    //         break;
+    //     case '/contact':
+    //         path += 'contact.html';
+    //         res.statusCode = 200;
+    //         break;
+    //     // Redirect page
+    //     case '/about-us':
+    //         res.statusCode = 301;
+    //         res.setHeader('Location', '/about');
+    //         res.end();
+    //         break;
+    //     default:
+    //         path += '404.html';
+    //         res.statusCode = 404;
+    //         break;
+    // }
     
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end();
-        } else {
-            // res.write(data);
-            res.end(data);
-        }
-    })
+    // fs.readFile(path, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //         res.end();
+    //     } else {
+    //         // res.write(data);
+    //         res.end(data);
+    //     }
+    // })
 });
 
 server.listen(port, hostName, () => {
@@ -62,32 +76,5 @@ const con = mysql.createConnection({
 
 con.connect((err) => {
     if (err) throw err;
-    console.log('Connected !');
+    // console.log('Connected !');
 });
-
-
-
-// const hostName = 'localhost';
-// const port = 3030;
-
-
-// // Create a server object
-// const server = http.createServer((req, res) => {
-
-//     if(req.url.endsWith('.html')) {
-//         const htmlFile = req.url.slice(1);
-//         fs.stat(`./${htmlFile}`, (err, stats) => {
-//             res.statusCode = 200;
-//             res.setHeader('Content-Type', 'text/html');
-//             if(stats) {
-//                 fs.createReadStream(htmlFile).pipe(res);
-//             } else {
-//                 res.statusCode = 404;
-//                 res.end('Sorry, page not found');
-//             }
-//         });
-//     }
-// });
-
-
-
