2276fd89f5272f413d01bd101f1b7d7648969bc0
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-12-21 14:27:32 +0100

    changTheRoutingCode

--- index.js
+++ index.js
@@ -3,30 +3,54 @@ const mysql = require('mysql');
 const url = require('url'); 
 const fs = require('fs');
 
-consolenst = hostname = '127.0.0.1';
+
+const hostName = 'localhost';
 const port = 3000;
 
-
-// Create a server object
 const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
 
-    if(req.url.endsWith('.html')) {
-        const htmlFile = req.url.slice(1);
-        fs.stat(`./${htmlFile}`, (err, stats) => {
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            if(stats) {
-                fs.createReadStream(htmlFile).pipe(res);
-            } else {
-                res.statusCode = 404;
-                res.end('Sorry, page not found');
-            }
-        });
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/contact':
+            path += 'contact.html';
+            res.statusCode = 200;
+            break;
+        // Redirect page
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
     }
+    
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    })
 });
 
-server.listen(port, hostname, () => {
-    console.log(`http://${hostname}:${port}`);
+server.listen(port, hostName, () => {
+    console.log(`Listening for requests http://${hostName}:${port}`);
 });
 
 const con = mysql.createConnection({
@@ -40,4 +64,30 @@ con.connect((err) => {
     if (err) throw err;
     console.log('Connected !');
 });
+
+
+
+// const hostName = 'localhost';
+// const port = 3030;
+
+
+// // Create a server object
+// const server = http.createServer((req, res) => {
+
+//     if(req.url.endsWith('.html')) {
+//         const htmlFile = req.url.slice(1);
+//         fs.stat(`./${htmlFile}`, (err, stats) => {
+//             res.statusCode = 200;
+//             res.setHeader('Content-Type', 'text/html');
+//             if(stats) {
+//                 fs.createReadStream(htmlFile).pipe(res);
+//             } else {
+//                 res.statusCode = 404;
+//                 res.end('Sorry, page not found');
+//             }
+//         });
+//     }
+// });
+
+
 
